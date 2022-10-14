d591816ccd644265e3883a5a9cdc0505c1a44524
Aye Chan Han <ayechan.han@mo.com.mm>
2022-10-14 11:14:59 +0630

    Created login interface, main page and navbar in React

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
-function App() {
+import React from "react";
+import socketIO from "socket.io-client";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Main from "./components/Main";
+import Home from "./components/Home";
+
+const socket = socketIO.connect("http://localhost:4000");
+
+const App = () => {
   return(
-    <div>
-      <p>Hello World!</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/app" element={<Main socket={socket}/>} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
+
 export default App;
